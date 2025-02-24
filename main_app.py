78352d0f17d1cccd71b4b41f6ae577f44edea256
Izzet <yunusovizzet6@gmail.com>
2025-02-24 17:32:30 +0100

    changed main

--- main_app.py
+++ main_app.py
@@ -4,12 +4,12 @@ from local_save_queries import initialize_sqlite_db, save_search_query, get_popu
 
 # ANSI коды для изменения цвета и стиля текста
 RESET = "\033[0m"
-RED = "\033[31m"
-GREEN = "\033[32m"
-
 BOLD = "\033[1m"
-YELLOW_UNDERLINE = "\033[33;4m"
+BOLD_RED = "\033[1;31m"
+BOLD_GREEN = "\033[1;32m"
+BOLD_YELLOW_UNDERLINE = "\033[1;33;4m"
 BOLD_GREEN_BACKGROUND = "\033[1;42m"
+
 
 
 def print_formated(text, format):
@@ -22,16 +22,16 @@ def main():
     connect = connect_to_db()
     if connect:
         print()
-        print_formated("Соединение с базой данных MySQL УСТАНОВЛЕНО.", BOLD_GREEN_BACKGROUND)
+        print_formated("☑️ Соединение с базой данных MySQL УСТАНОВЛЕНО", BOLD_GREEN_BACKGROUND)
 
     initialize_sqlite_db()
 
     while True:
-        print_formated("\nВыберите действие:", YELLOW_UNDERLINE)
-        print_formated("1. Поиск фильмов по ключевому слову", BOLD)
-        print_formated("2. Поиск фильмов по жанру и году", BOLD)
-        print_formated("3. Показать самые популярные запросы", BOLD)
-        print_formated("4. Выйти", BOLD)
+        print_formated("\nЛегендарное меню действий! Выберите мудро:", BOLD_YELLOW_UNDERLINE)
+        print_formated("1. Искать фильмы по ключевому слову (как настоящий детектив 🕵️‍♂️)", BOLD)
+        print_formated("2. Искать фильмы по жанру и году (как квест, но полегче 🎬)", BOLD)
+        print_formated("3. Посмотреть популярные запросы 🍿", BOLD)
+        print_formated("4. Самоуничтожиться… шучу, просто выйти из программы 😎", BOLD)
 
         choice = input("\nВведите номер действия: ")
 
@@ -42,7 +42,13 @@ def main():
             display_movies(movies)
 
         elif choice == "2":
-            genre = input("Введите жанр: ")
+            print_formated("Список доступных жанров:", BOLD_YELLOW_UNDERLINE)
+            print("Action, Animation, Children, Classics, Comedy, Documentary,"
+                " Drama, \nFamily, Foreign, Games, Horror, Music, New, Sci-Fi, Sports, Travel")
+            print_formated("Годы выпуска:", BOLD_YELLOW_UNDERLINE)
+            print("1980 - 2023")
+
+            genre = input("\nВведите жанр: ")
             year = input("Введите год: ")
             movies = search_movies_by_genre_and_year(genre, year)
             save_search_query(f"{genre} {year}")
@@ -53,27 +59,27 @@ def main():
             display_popular_queries(queries)
 
         elif choice == "4":
-            print_formated("Выход из программы.", RED)
+            print_formated("Выход из программы.", BOLD_RED)
             break
 
         else:
-            print_formated("Неверный выбор. Пожалуйста, попробуйте снова.", RED)
+            print_formated("Неверный выбор. Пожалуйста, попробуйте снова.", BOLD_RED)
 
 def display_movies(movies):
     if movies:
-        print_formated(f"\nНайдено фильмов: {len(movies)}", GREEN)
+        print_formated(f"\nНайдено фильмов: {len(movies)}", BOLD_GREEN)
         for movie in movies:
             print(f'Название: "{movie.get("title")}",  Год выпуска: {movie.get("release_year")}')
     else:
-        print_formated("\nФильмы не найдены.", RED)
+        print_formated("\nФильмы не найдены.", BOLD_RED)
 
 def display_popular_queries(queries):
     if queries:
-        print_formated("Самые популярные запросы:", GREEN)
+        print_formated("Самые популярные запросы:", BOLD_GREEN)
         for query in queries:
             print(f"Запрос: {query[0]}, Количество: {query[1]}")
     else:
-        print_formated("\nПопулярные запросы не найдены.", RED)
+        print_formated("\nПопулярные запросы не найдены.", BOLD_RED)
 
 if __name__ == "__main__":
     main()
